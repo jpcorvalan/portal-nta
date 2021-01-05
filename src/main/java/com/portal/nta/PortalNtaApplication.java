e107ebb8ec959de1c16d3a2a3e26bdcdf24156f7
jpcorvalan <76654210+jpcorvalan@users.noreply.github.com>
2021-01-05 12:43:08 -0300

    Uploading files

---
+++ src/main/java/com/portal/nta/PortalNtaApplication.java
@@ -0,0 +1,13 @@
+package com.portal.nta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalNtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalNtaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portal/nta/PortalNtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.nta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalNtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
